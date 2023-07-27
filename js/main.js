865a2e9b9a363f53816608841d4064c5b7868490
Олексій <shevchukos@MacBook-Air-Oleksij.local>
2023-07-27 19:08:15 +0300

    Add TELENKOV button ,which return main screen

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 const sections = document.querySelectorAll(".section");
 const tabs = document.querySelectorAll(".main-menu__item");
 const mainSection = document.querySelector(".main-section");
+const mainScreen = document.querySelector(".telenkov");
 
-function selectTab(num) {
-    mainSection.classList.remove("active-section");
+function returnMainScreen() {
+    closeAllSection();
+    mainSection.classList.add("active-section");
+};
+function closeAllSection() {
     for(let i=0;i<tabs.length;i++){
         tabs[i].classList.remove("item__active");
         sections[i].classList.remove("active-section");
     }
+}
+function selectTab(num) {
+    mainSection.classList.remove("active-section");
     tabs[num].classList.add("item__active");
     sections[num].classList.add("active-section");
 };
+
 tabs.forEach((item,indexTab) => {
     item.addEventListener('click', () => {
+        closeAllSection();
         selectTab(indexTab);
     })
+});
+mainScreen.addEventListener("click", () => {
+    returnMainScreen();
 });
