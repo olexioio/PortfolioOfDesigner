e3c037759136827d22de4ce8f9ebab0fda7b6c26
Олексій <shevchukos@MacBook-Air-Oleksij.local>
2023-07-31 04:56:54 +0300

    Add validation function, now everything working

--- js/validation.js
+++ js/validation.js
@@ -1,56 +1,39 @@
-// INPUT EMAIL
-const portfolioEmail = document.getElementById("portfolio-email");
-const careerEmail = document.getElementById("career-email");
-const requestEmail = document.getElementById("request-email");
-
-// SUBMIT BUTTON
-const portfolioSubmit = document.getElementById("portfolio-submit");
-
-// FORMS
-const portfolioForm = document.getElementById("form-portfolio");
-
-
-// EVENT LISTENER ON INPUT EMAIL
-portfolioEmail.addEventListener("input",updateInputPortfolio);
-careerEmail.addEventListener("input",updateInputCareer);
-requestEmail.addEventListener("input",updateInputRequest);
-
-// VALIDATIONS OF EMAIL
-function updateInputPortfolio() {
-    if(validateEmail(portfolioEmail.value) && portfolioEmail.value !== 0){
-        console.log("true");
-        portfolioEmail.classList.remove("_error");
-        return true;
-    }else{
-        console.log("false");
-        portfolioEmail.classList.add("_error")
-        return false;
-    ;}
-};
-function updateInputCareer() {
-    if(validateEmail(careerEmail.value) && careerEmail.value !== 0){
-        careerEmail.classList.remove("_error");
-        return true;
-    }else{
-        careerEmail.classList.add("_error")
-        return false;
-    ;}
-};
-function updateInputRequest() {
-    if(validateEmail(requestEmail.value) && requestEmail.value !== 0){
-        requestEmail.classList.remove("_error");
-        return true;
-    }else{
-        requestEmail.classList.add("_error")
-        return false;
-    ;}
-};
-const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-function validateEmail(value) {
-    return EMAIL_REGEXP.test(value);
-};
-
-// SEND FORM FUNCTIONS
-portfolioForm.addEventListener("submit", (e) => {
-    if(updateInputPortfolio === false) e.preventDefault();
+// FORM get by ID
+let portfolioForm = document.getElementById("form-portfolio");
+let careerForm = document.getElementById("form-career");
+let requestForm = document.getElementById("form-request");
+// INPUT get by ID
+let portfolioEmail = document.getElementById("portfolio-email");
+let careerEmail = document.getElementById("career-email");
+let requestEmail = document.getElementById("request-email");
+// EVENT LISTENER by SUBMIT
+portfolioForm.addEventListener("submit", function submitPortfolioForm(e) {
+    e.preventDefault();
+    if(portfolioEmail.value === "") {
+        portfolioEmail.classList.add("_error");
+    }else if(!validateEmail(portfolioEmail)){
+        portfolioEmail.classList.add("_error");
+    }else portfolioEmail.classList.remove("_error");
 });
+careerForm.addEventListener("submit", function submitCareerForm(e) {
+    e.preventDefault();
+    if(careerEmail.value === "") {
+        careerEmail.classList.add("_error");
+    }else if(!validateEmail(careerEmail)){
+        careerEmail.classList.add("_error");
+    }else careerEmail.classList.remove("_error");
+});
+requestForm.addEventListener("submit", function submitRequestForm(e) {
+    e.preventDefault();
+    if(requestEmail.value === "") {
+        requestEmail.classList.add("_error");
+    }else if(!validateEmail(requestEmail)){
+        requestEmail.classList.add("_error");
+    }else requestEmail.classList.remove("_error");
+});
+
+// VALIDATION FUNCTION
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]+)$/iu;
+function validateEmail(email) {
+    return EMAIL_REGEXP.test(email.value);
+};
