97b57c38e089b06f6cf56e8de3f34329e8cda745
Олексій <shevchukos@MacBook-Air-Oleksij.local>
2023-08-03 20:00:45 +0300

    Add animations

---
+++ js/mobile-menu-animation.js
@@ -0,0 +1,15 @@
+const openMobileMenu = document.querySelector(".mobile-menu");
+const mobileMenu = document.querySelector(".mobile-menu-module");
+const closeMobileMenu = document.getElementById("close-mobile-menu");
+
+// openMobileMenu.addEventListener("click", () => {
+//     mobileMenu.style.animation = "mobile-menu-open 0.25s ease-in-out forwards";
+// });
+
+// closeMobileMenu.addEventListener("click", () => {
+//     mobileMenu.style.animation = "";
+//     // mobileMenu.style.animation = "mobile-menu-close 0.25s ease-in-out forwards";
+//     // console.log("adfsfsf");
+// });
+
+// console.log(closeMobileMenu);
