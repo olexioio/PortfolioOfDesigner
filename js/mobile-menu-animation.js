534f2dd00e4187ccd913d7f7a97437e0c5d6071f
Олексій <shevchukos@MacBook-Air-Oleksij.local>
2023-08-04 18:35:51 +0300

    Create mobile tab-menu

--- js/mobile-menu-animation.js
+++ js/mobile-menu-animation.js
@@ -1,10 +1,48 @@
 const mobileMenu = document.getElementById("mobile-menu-module");
 const mobileMenuButton = document.getElementById("mobile-menu");
-const mobileMenuClose = document.getElementById("close-mobile-menu")
+const mobileMenuClose = document.getElementById("close-mobile-menu");
 
 mobileMenuButton.addEventListener("click", () => {
     mobileMenu.classList.add("mobile-menu-module-active");
 });
 mobileMenuClose.addEventListener("click", () => {
-    setTimeout(()=>{mobileMenu.classList.remove("mobile-menu-module-active");},0);
+    setTimeout(()=>{mobileMenu.classList.remove("mobile-menu-module-active");},2.6);
+});
+
+// MOBILE TAB
+const portfolioMobile = document.querySelector(".portfolio-mobile");
+const careerMobile = document.querySelector(".career-mobile");
+const requestMobile = document.querySelector(".request-mobile");
+const mobileSections = document.querySelectorAll(".mobile-section");
+// ITEMS
+const mobileItem = document.querySelectorAll(".mobile-menu__item");
+// main screen
+const offer = document.querySelector(".offer-mob");
+const offerBtn = document.querySelector(".btn-wrapper-mobile");
+// 
+const mainScreenLink = document.querySelector(".telenkov-mob");
+
+mobileItem.forEach((item,indexTab) => {
+    item.addEventListener("click", () => {
+        closeAllMobileSection();
+        selectMobileTab(indexTab);
+        setTimeout(()=>{mobileMenu.classList.remove("mobile-menu-module-active");},2.6);
+    })
+});
+function closeAllMobileSection() {
+    offer.classList.remove("active-mobile-section");
+    offerBtn.classList.remove("active-mobile-section");
+    for(let i=0;i<mobileItem.length;i++){
+        mobileItem[i].classList.remove("mobile-item__active");
+        mobileSections[i].classList.remove("active-mobile-section");
+    }
+};
+function selectMobileTab(num) {
+    mobileItem[num].classList.add("mobile-item__active");
+    mobileSections[num].classList.add("active-mobile-section");
+};
+mainScreenLink.addEventListener("click", () => {
+    closeAllMobileSection();
+    offer.classList.add("active-mobile-section");
+    offerBtn.classList.add("active-mobile-section");
 });
--- js/validation.js
+++ js/validation.js
@@ -2,6 +2,10 @@
 let portfolioForm = document.getElementById("form-portfolio");
 let careerForm = document.getElementById("form-career");
 let requestForm = document.getElementById("form-request");
+
+let portfolioFormMobile = document.getElementById("form-portfolio-mobile");
+let careerFormMobile = document.getElementById("form-career-mobile");
+let requestFormMobile = document.getElementById("form-request-mobile");
 // VALID TEXT
 let portfolidoValidMessage = document.querySelector("portfolio-valid");
 let careerValidMessage = document.querySelector("career-valid");
@@ -10,7 +14,13 @@ let requestValidMessage = document.querySelector("request-valid");
 let portfolioEmail = document.getElementById("portfolio-email");
 let careerEmail = document.getElementById("career-email");
 let requestEmail = document.getElementById("request-email");
+
+let portfolioEmailMobile = document.getElementById("portfolio-email-mobile");
+let careerEmailMobile = document.getElementById("career-email-mobile");
+let requestEmailMobile = document.getElementById("request-email-mobile");
 // EVENT LISTENER by SUBMIT
+
+// PORTFOLIO FORM DESKTOP and MOBILE
 portfolioForm.addEventListener("submit", function submitPortfolioForm(e) {
     e.preventDefault();
     let date = new Date();
@@ -30,6 +40,26 @@ portfolioForm.addEventListener("submit", function submitPortfolioForm(e) {
         portfolioEmail.previousElementSibling.classList.remove("active");
     };
 });
+portfolioFormMobile.addEventListener("submit", function submitPortfolioForm(e) {
+    e.preventDefault();
+    // let date = new Date();
+    // const dateOutValid = date.getSeconds()+4;
+    if(portfolioEmailMobile.value === "") {
+        portfolioEmailMobile.classList.add("_error");
+        portfolioEmailMobile.previousElementSibling.classList.add("active");
+        setTimeout(portfolioValid,2000);
+    }else if(!validateEmail(portfolioEmailMobile)){
+        portfolioEmailMobile.classList.add("_error");
+        portfolioEmailMobile.previousElementSibling.classList.add("active");
+        setTimeout(portfolioValid,2000);
+    }else {portfolioEmailMobile.classList.remove("_error");portfolioEmailMobile.previousElementSibling.classList.remove("active");};
+    
+    function portfolioValid(){
+        portfolioEmailMobile.classList.remove("_error");
+        portfolioEmailMobile.previousElementSibling.classList.remove("active");
+    };
+});
+// CAREER FORM DESKTOP and MOBILE
 careerForm.addEventListener("submit", function submitCareerForm(e) {
     e.preventDefault();
     if(careerEmail.value === "") {
@@ -47,6 +77,24 @@ careerForm.addEventListener("submit", function submitCareerForm(e) {
         careerEmail.previousElementSibling.classList.remove("active");
     }
 });
+careerFormMobile.addEventListener("submit", function submitCareerForm(e) {
+    e.preventDefault();
+    if(careerEmailMobile.value === "") {
+        careerEmailMobile.classList.add("_error");
+        careerEmailMobile.previousElementSibling.classList.add("active");
+        setTimeout(creerValid,2000);
+    }else if(!validateEmail(careerEmailMobile)){
+        careerEmailMobile.classList.add("_error");
+        careerEmailMobile.previousElementSibling.classList.add("active");
+        setTimeout(creerValid,2000);
+    }else {careerEmailMobile.classList.remove("_error");careerEmailMobile.previousElementSibling.classList.remove("active");};
+    
+    function creerValid(){
+        careerEmailMobile.classList.remove("_error");
+        careerEmailMobile.previousElementSibling.classList.remove("active");
+    }
+});
+// REQUEST FORM DESKTOP and MOBILE
 requestForm.addEventListener("submit", function submitRequestForm(e) {
     e.preventDefault();
     if(requestEmail.value === "") {
@@ -64,18 +112,26 @@ requestForm.addEventListener("submit", function submitRequestForm(e) {
         requestEmail.previousElementSibling.classList.remove("active");
     }
 });
+requestFormMobile.addEventListener("submit", function submitRequestForm(e) {
+    e.preventDefault();
+    if(requestEmailMobile.value === "") {
+        requestEmailMobile.classList.add("_error");
+        requestEmailMobile.previousElementSibling.classList.add("active");
+        setTimeout(requestValid,2000);
+    }else if(!validateEmail(requestEmailMobile)){
+        requestEmailMobile.classList.add("_error");
+        requestEmailMobile.previousElementSibling.classList.add("active");
+        setTimeout(requestValid,2000);
+    }else {requestEmailMobile.classList.remove("_error");requestEmailMobile.previousElementSibling.classList.remove("active");}
+    
+    function requestValid(){
+        requestEmailMobile.classList.remove("_error");
+        requestEmailMobile.previousElementSibling.classList.remove("active");
+    }
+});
 
 // VALIDATION FUNCTION
 const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]+)$/iu;
 function validateEmail(email) {
     return EMAIL_REGEXP.test(email.value);
 };
-
-let date = new Date();
-// console.log(date);
-// console.log(date.getSeconds());
-// console.log(date.getSeconds()+10);
-let dateStatic = date.getSeconds()+2;
-console.log(date.getSeconds())
-console.log(dateStatic)
-if(date.getSeconds()===dateStatic){console.log("dsgdfgfdg")}
